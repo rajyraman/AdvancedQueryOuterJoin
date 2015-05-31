adfff3f2ee82dbb7c820edb29124ad476672efc7
rajyraman <rajyraman@gmail.com>
2015-05-31 17:21:51 +1000

    Initial commit

---
+++ RYR.OuterJoin/OuterJoinPlugin.cs
@@ -0,0 +1,48 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace RYR.OuterJoin
+{
+    public class OuterJoinPlugin : Plugin
+    {
+        public OuterJoinPlugin()
+            : base(typeof(OuterJoinPlugin))
+        {
+            base.RegisteredEvents.Add(new Tuple<int, string, string, Action<LocalPluginContext>>(
+                (int)LocalPluginContext.PipelinePhase.PreOperation,
+                LocalPluginContext.Message.RetrieveMultiple.ToString(),
+                null, Execute));
+        }
+
+        protected void Execute(LocalPluginContext localContext)
+        {
+            if (localContext == null)
+            {
+                throw new ArgumentNullException("localContext");
+            }
+            if (!localContext.PluginExecutionContext.InputParameters.ContainsKey("Query")) return;
+
+            var query = localContext.PluginExecutionContext.InputParameters["Query"] as QueryExpression;
+            if (query != null)
+            {
+                foreach (var linkedEntity in query.LinkEntities)
+                {
+                    var linkPrimaryKey = linkedEntity.LinkToEntityName + "id";
+                    if (linkedEntity.LinkCriteria.Conditions
+                        .Any(x => x.AttributeName
+                            .Equals(linkPrimaryKey, StringComparison.CurrentCultureIgnoreCase)
+                            && x.Operator == ConditionOperator.Null))
+                    {
+                        query.Criteria.AddCondition(linkedEntity.LinkToEntityName, linkPrimaryKey, ConditionOperator.Null);
+                        linkedEntity.JoinOperator = JoinOperator.LeftOuter;
+                        linkedEntity.LinkCriteria.Conditions.Clear();
+                    }
+                } 
+            }
+        }
+    }
+}
